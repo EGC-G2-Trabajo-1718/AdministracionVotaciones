427c5a3558a94feaef0292f96494fd6ff67312da
martos93 <jesusma93@gmail.com>
2017-12-17 13:45:01 +0100

    add: Tests de Votación
    Se añade al proyecto el archivo junit.xml para la realización de tests.
    Se añade la clase AbstractTest.java de la que extenderán los tests.
    Se añaden los tests de crear, borrar, editar y obtener votación. #17

---
+++ src/test/java/utilities/AbstractTest.java
@@ -0,0 +1,41 @@
+/*
+ * SampleTest.java
+ *
+ * Copyright (C) 2017 Universidad de Sevilla
+ *
+ * The use of this project is hereby constrained to the conditions of the
+ * TDG Licence, a copy of which you may download from
+ * http://www.tdg-seville.info/License.html
+ */
+
+package utilities;
+
+import org.junit.After;
+import org.junit.Before;
+
+public abstract class AbstractTest {
+
+	// Supporting services --------------------------------
+
+	// Set up and tear down -------------------------------
+
+	@Before
+	public void setUp() {
+	}
+
+	@After
+	public void tearDown() {
+	}
+
+	// Supporting methods ---------------------------------
+
+	public void checkExceptions(final Class<?> expected, final Class<?> caught) {
+		if (expected != null && caught == null)
+			throw new RuntimeException(expected.getName() + " was expected");
+		else if (expected == null && caught != null)
+			throw new RuntimeException(caught.getName() + " was unexpected");
+		else if (expected != null && caught != null && !expected.equals(caught))
+			throw new RuntimeException(expected.getName() + " was expected, but " + caught.getName() + " was thrown");
+	}
+
+}
